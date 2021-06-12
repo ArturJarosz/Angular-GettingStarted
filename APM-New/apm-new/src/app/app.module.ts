d25ef5eda3afc91d195ec902e604e39828396d9d
Artur Jarosz <azjarosz@gmail.com>
2021-06-12 21:48:22 +0200

    Module06

--- APM-New/apm-new/src/app/app.module.ts
+++ APM-New/apm-new/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.compontent';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
